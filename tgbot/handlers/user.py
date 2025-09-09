91b33c0c7fe3ac58add29993e80ac163a06fa8d8
mirodil <ortikov.97@gmail.com>
2025-09-09 11:14:06 +0500

    start command

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -16,6 +16,10 @@ async def user_start(message: Message, state: FSMContext, repo: RequestsRepo):
     await state.clear()
     submission = await repo.submissions.get_submission_by_tguser_id(message.from_user.id)
     if not submission:
-        await message.reply("Xush kelibsiz!", reply_markup=register_user_keyboard())
+        await message.reply(
+            "“Media Start” loyihasining nizomi bilan tanishib chiqing!\n\n"
+            "Loyiha nizomi: https://telegra.ph/Media-start-loji%D2%B3asi-NIZOMI-09-09",
+            reply_markup=register_user_keyboard(),
+        )
     else:
         await message.reply("Siz allaqachon ro'yxatdan o'tgansiz.", reply_markup=ReplyKeyboardRemove())
